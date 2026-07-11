352ae891aa85b9be65dc45b145e6ae9ac7801e77
chamathkadinidu08-oss <chamathkadinidu08@gmail.com>
2026-07-11 13:36:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DAINIYA-MD/DINIYA-MD/blob/main/images/DINIYA-MD.png?raw=true",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DINIYA-MD Is Alive Now😍*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
 
 
