8690eff408d58b653a5d090167764f16a35f1314
chamathkadinidu08-oss <chamathkadinidu08@gmail.com>
2026-07-11 13:37:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DAINIYA-MD/DINIYA-MD/blob/main/images/DINIYA-MD.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DINIYA-MD Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+BOT_OWNER: '94707435575',  // Replace with the owner's phone number
 
 
 
